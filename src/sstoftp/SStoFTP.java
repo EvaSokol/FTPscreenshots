ddef1ff8ec3efb79f329921033bb36749908ae09
EvaSokol <esokolyanskaya@lohika.com>
2014-09-30 18:16:32 +0300

    Compound path support added

--- src/sstoftp/SStoFTP.java
+++ src/sstoftp/SStoFTP.java
@@ -7,10 +7,12 @@ import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Iterator;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import sun.net.ftp.FtpClient;
 import sun.net.ftp.FtpDirEntry;
 import sun.net.ftp.FtpProtocolException;
@@ -27,11 +29,12 @@ public class SStoFTP {
 		Driver.get(BaseUrl);
 		
 	//Using our method
-		FTPscreenshots("asd1", "zxc1");
+		FTPscreenshots("asd_filename", "qqq/fff/zxc/abc");
 		
 	//Close screenshot source 
 		Driver.close();
-	}
+		
+		}
 	
 	public static void FTPscreenshots(String filename, String ftpfolder) {
 
@@ -51,24 +54,39 @@ public class SStoFTP {
 			File ssf = ((TakesScreenshot)Driver).getScreenshotAs(OutputType.FILE);
 			InputStream fis = new FileInputStream(ssf);
 			
-	//Checking if directory exists 
-			Iterator <FtpDirEntry> it = fc.listFiles("/"); 
+	// Path analyse, create and go into ftp directory
 			
-			FtpDirEntry dir;
+			String[] way =  ftpfolder.split("[/]");
+			
 			boolean dirExists = false;
+			FtpDirEntry dir;
+			Iterator <FtpDirEntry> it;
+			String currentDir = "/";
 			
-			while (it.hasNext() ) {
-				dir = (FtpDirEntry)it.next();
-				if (dir.getName().compareTo(ftpfolder) == 0)
-					dirExists = true;
-			};
+			for (int i=0; i<way.length; i++)
+			{
+				
+				it = fc.listFiles(currentDir);
+								
+				dirExists = false;
+				
+				while (it.hasNext() ) {
+					dir = (FtpDirEntry)it.next();
+					if (dir.getName().compareTo(way[i]) == 0)
+						dirExists = true;
+				};
+				
+				if (dirExists == false)
+					fc.makeDirectory(way[i]);
 			
-			if (dirExists == false)
-				fc.makeDirectory(ftpfolder);
+				fc.changeDirectory(way[i]);
+				currentDir += "/" + way[i];
+				
+			}	
 			
 	//Put screenshot to folder on ftp
 			
-			fc.putFile(ftpfolder + "/" + filename + ".png", fis);
+			fc.putFile(filename + ".png", fis);
 		
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
@@ -78,8 +96,16 @@ public class SStoFTP {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-      
+	      
 	}
 	
+	static void gotoFolder(String path) {
+		
+		String[] way =  path.split("[/]");
+		for (int i=0; i<way.length; i++)
+		{
+			System.out.println(way[i]);
+		}
+	}
 
 }
